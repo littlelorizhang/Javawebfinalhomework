97149b40cd5bf9f759f9bba9397c95676e744587
littlelorizhang <63146003+littlelorizhang@users.noreply.github.com>
2022-05-24 21:19:03 +0800

    Update DeleteEmployeeServlet.java

--- src/managerservlet/DeleteEmployeeServlet.java
+++ src/managerservlet/DeleteEmployeeServlet.java
@@ -30,6 +30,33 @@ public class DeleteEmployeeServlet extends HttpServlet {
         try {
             boolean b=dao.delete(uid);
             if(b) {
+                String ip = request.getHeader("X-Forwarded-For");
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getHeader("Proxy-Client-IP");
+                }
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getHeader("WL-Proxy-Client-IP");
+                }
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getHeader("HTTP_CLIENT_IP");
+                }
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+                }
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getRemoteAddr();
+                }
+
+                Date date = new Date();
+                SimpleDateFormat dateFormat= new SimpleDateFormat("yyyy-MM-dd :hh:mm:ss");
+                String time=dateFormat.format(date);
+
+                Operate operate=new Operate();
+                operate.setO_uid(user.getUid());
+                operate.setOip(ip);
+                operate.setOtime(time);
+                operate.setOtype("删除销售人员");
+                operate.setOid(RandomStringUtils.randomNumeric(10));
                 request.getSession().setAttribute("user",user);
                 request.getSession().setAttribute("message","操作成功");
                 request.getRequestDispatcher("/managers/index3.jsp").forward(request,response);
